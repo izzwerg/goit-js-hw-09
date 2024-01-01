f14bd92715ab1ad81317bd6db09d3c75157c30c0
izzwerg <izzwerg.kr@gmail.com>
2024-01-02 01:06:05 +0200

    another commit

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -1,6 +1,6 @@
-// import SimpleLightbox from 'simplelightbox';
+import SimpleLightbox from 'simplelightbox';
 
-// import 'simplelightbox/dist/simple-lightbox.min.css';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const images = [
   {
