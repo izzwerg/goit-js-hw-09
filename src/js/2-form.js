213a15b3daae58601f73047b8b8b1f4bf584f9a1
izzwerg <izzwerg.kr@gmail.com>
2024-01-02 14:04:01 +0200

    Update 2-form.js

---
+++ src/js/2-form.js
@@ -0,0 +1,32 @@
+const form = document.querySelector('.feedback-form');
+const emailField = form.querySelector('input');
+const messageField = form.querySelector('textarea');
+const localStorageKey = 'feedback-form-state';
+
+let localStorageData = { email: '', message: '' };
+
+fillForm();
+
+form.addEventListener('submit', submitFunction);
+
+function submitFunction(e) {
+  e.preventDefault();
+  console.log(localStorage.getItem(localStorageKey));
+  localStorage.removeItem(localStorageKey);
+  form.reset();
+  localStorageData = { email: '', message: '' };
+}
+
+form.addEventListener('input', e => {
+  localStorageData[e.target.name] = e.target.value;
+  localStorage.setItem(localStorageKey, JSON.stringify(localStorageData));
+});
+
+function fillForm() {
+  const localStorageSave = JSON.parse(localStorage.getItem(localStorageKey));
+  if (localStorageSave) {
+    localStorageData = localStorageSave;
+    emailField.value = localStorageSave.email;
+    messageField.value = localStorageSave.message;
+  }
+}
